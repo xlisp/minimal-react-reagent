026dabb24b14655f6db389601da26a331e852224
chanshunli <chanshunli@gmail.com>
2019-07-30 01:07:16 +0800

    https://github.com/bvaughn/react-window Cljs和React的互操作

--- src/foo/core.cljs
+++ src/foo/core.cljs
@@ -8,3 +8,10 @@
 ;; :export metadata is necessary.
 (def ^:export MyComponent
   (reagent/reactify-component my-component))
+
+(defn my-component-2 [props]
+  [:p ;;{:style "red"}: The `style` prop expects a mapping from style properties to values, not a string. For example, style={{marginRight: spacing + 'em'}} when using JSX.
+   (str "cljs-component: " (:name props))])
+
+(def ^:export MyComponent2
+  (reagent/reactify-component my-component-2))
--- src/index.js
+++ src/index.js
@@ -4,7 +4,40 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// react-window无限下拉:
+import { FixedSizeList as List } from "react-window";
+import AutoSizer from "react-virtualized-auto-sizer";
+
+import "./styles.css";
+
+const MyComponent2 = window.foo.core.MyComponent2;
+
+const Row = ({ index, style }) => (
+  <div className={index % 2 ? "ListItemOdd" : "ListItemEven"} style={style}>
+      React {index} Row ,
+      <MyComponent2 name={index}/>
+  </div>
+);
+
+const Example = () => (
+  <AutoSizer>
+    {({ height, width }) => (
+      <List
+        className="List"
+        height={height}
+        itemCount={1000}
+        itemSize={35}
+        width={width}
+      >
+        {Row}
+      </List>
+    )}
+  </AutoSizer>
+);
+// ///
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Example />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
